d48dfe8d66647e280d590ae7e29ea4f68aa6e56c
DESKTOP-N6HFU1P\HoaDang <hoadang0107@gmail.com>
2018-04-13 22:27:54 +0700

    gd
    giao diện

---
+++ SinhVien/app/src/main/java/com/example/hoadang/sinhvien/ActivityLoginSV.java
@@ -0,0 +1,13 @@
+package com.example.hoadang.sinhvien;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityLoginSV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sv);
+    }
+}
